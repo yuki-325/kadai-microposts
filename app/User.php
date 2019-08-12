608af15418ca6c076f99028090cb0a4d8993bedf
yuki.nakano <sakeikura325@yahoo.co.jp>
2019-08-12 00:19:22 +0000

    user registration

--- app/User.php
+++ app/User.php
@@ -17,6 +17,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
+    
+    
 
     /**
      * The attributes that should be hidden for arrays.
